bbb8be17ddd4c1c2923d5cf5dee440d0dea59c76
ShimaSama <47900457+ShimaSama@users.noreply.github.com>
2019-03-11 18:39:01 +0100

    Add files via upload

---
+++ email.py
@@ -0,0 +1,15 @@
+#code to know if the email format is correct or not
+email=input("Introduce an email address: ")
+
+count=email.count('@')
+count2=email.count('.')
+
+
+end=email.rfind('@') #final
+start=email.find("@")
+
+if(count!=1 or count<1 or end==(len(email)-1) or start==0):
+	print("Wrong email address")	
+
+else:
+	print("correct email address")
